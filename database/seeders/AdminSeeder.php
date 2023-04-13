64fa0c40d3cd93ffad9d592d421114fa22d37d57
Ynsoft-Dev <ynsoft@live.fr>
2023-04-13 16:11:00 +0100

    creation de l'adminSeeder et migration fresh avec seed

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+        'name' => 'admin',
+        'email' => 'admin@admin.com',
+        'password' => bcrypt('adminadmin')
+        ]);
+
+        $role = Role::create(['name' => 'Admin']);
+        $permissions = Permission::pluck('id','id')->all();
+        $role->syncPermissions($permissions);
+        $user->assignRole([$role->id]);
+    }
+}
